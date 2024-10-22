7174eaf96ea02ada8c143d3263480ed5caefa566
Tiramisu <165537498+darwinx64@users.noreply.github.com>
2024-10-22 16:14:02 -0500

    Initial Commit

---
+++ Nin0chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nin0chat
+//
+//  Created by tiramisu on 10/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nin0chat/Nin0chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nin0chatApp.swift
+//  Nin0chat
+//
+//  Created by tiramisu on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Nin0chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
